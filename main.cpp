5a46452ba4c176812d0b82e36acc2694343312ab
khanhtrinh301103 <s3927152@rmit.edu.vn>
2023-09-07 22:49:20 +0700

    h

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "AdminLogin.h"
 #include "UserLogin.h"
 #include "RegisterAccount.h"
-#include "AdminUI.h" // Include the AdminUI header
+#include "AdminUI.h"
 
 using namespace std;
 
@@ -10,19 +10,18 @@ int main() {
     int choice;
 
     while (true) {
-        cout << "Welcome to the program!" << endl;
-        cout << "1. Login as Admin" << endl;
-        cout << "2. Login as Member" << endl;
-        cout << "3. Register" << endl;
-        cout << "4. Exit" << endl;
-        cout << "Enter your choice (1/2/3/4): ";
+        cout << "\nWelcome to the EliteMoto service!" << endl;
+        cout << "1. Login" << endl;
+        cout << "2. Register" << endl;
+        cout << "3. Exit" << endl;
+        cout << "Enter your choice (1/2/3): ";
 
         cin >> choice;
 
         switch (choice) {
             case 1:
                 {
-                    cout << "You selected 'Login as Admin'. Please enter your username: ";
+                    cout << "You selected 'Login'. Please enter your username: ";
                     string username;
                     cin >> username;
                     
@@ -33,30 +32,15 @@ int main() {
                     if (AdminLogin::isAdminLoginValid(username, password)) {
                         cout << "Admin login successful. Welcome, " << username << "!" << endl;
                         AdminUI::showMenu(); // Show the admin menu
+                    } else if (UserLogin::isUserLoginValid(username, password)) {
+                        cout << "User login successful. Welcome, " << username << "!" << endl;
+                        // Add user-specific functionality here
                     } else {
-                        cout << "Admin login failed. Please check your username and password." << endl;
+                        cout << "Login failed. Please check your username and password." << endl;
                     }
                 }
                 break;
             case 2:
-                {
-                    cout << "You selected 'Login as Member'. Please enter your username: ";
-                    string username;
-                    cin >> username;
-                    
-                    cout << "Please enter your password: ";
-                    string password;
-                    cin >> password;
-
-                    if (UserLogin::isUserLoginValid(username, password)) {
-                        cout << "User login successful. Welcome, " << username << "!" << endl;
-                        // Add user-specific functionality here
-                    } else {
-                        cout << "User login failed. Please check your username and password." << endl;
-                    }
-                }
-                break;
-            case 3:
                 {
                     cout << "You selected 'Register'. Please enter a new username: ";
                     string newUsername;
@@ -81,11 +65,11 @@ int main() {
                     }
                 }
                 break;
-            case 4:
+            case 3:
                 cout << "Exiting the program. Goodbye!" << endl;
                 return 0; // Exit the program
             default:
-                cout << "Invalid choice. Please enter a valid option (1/2/3/4)." << endl;
+                cout << "Invalid choice. Please enter a valid option (1/2/3)." << endl;
                 break;
         }
     }
