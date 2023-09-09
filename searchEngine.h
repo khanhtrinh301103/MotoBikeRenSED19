bec1242bfea04b2b8014d0fdb1aa299148f54185
khanhtrinh301103 <s3927152@rmit.edu.vn>
2023-09-09 22:37:56 +0700

    a

--- searchEngine.h
+++ searchEngine.h
@@ -9,7 +9,7 @@
 
 class SearchEngine {
 public:
-        static void searchMotorbikesByCity() {
+    static void searchMotorbikesByCity() {
         // Search for motorbikes in the specified city
         std::cout << "\nEnter the city to search for motorbikes: ";
         std::string searchCity;
@@ -21,11 +21,22 @@ public:
         std::vector<std::string> motorbikeDetails;
 
         std::string line;
+        bool cityStatementDisplayed = false; // To track if the city statement is displayed
 
         while (std::getline(productDetailFile, line)) {
             if (line.find("city: " + searchCity) != std::string::npos) {
                 foundMotorbikeInCity = true;
-                break; // Stop after finding the city line
+                // Display the city statement only once
+                if (!cityStatementDisplayed) {
+                    std::cout << "\nThe motorbikes available in " << searchCity << " city:\n" << std::endl;
+                    cityStatementDisplayed = true;
+                }
+                // Display motorbike details and add a blank line between motorbikes
+                for (const std::string& detail : motorbikeDetails) {
+                    std::cout << detail << std::endl;
+                }
+                std::cout << std::endl; // Add a blank line
+                motorbikeDetails.clear(); // Clear the details for the next motorbike
             }
             motorbikeDetails.push_back(line);
             if (motorbikeDetails.size() > 11) {
@@ -37,15 +48,9 @@ public:
 
         if (!foundMotorbikeInCity) {
             std::cout << "No available motorbikes found in " << searchCity << " City." << std::endl;
-        } else {
-            std::cout << "\nThe motorbikes available in " << searchCity << " city: \n" << std::endl;
-
-            // Display motorbike details and the preceding lines
-            for (const std::string& detail : motorbikeDetails) {
-                std::cout << detail << std::endl;
-            }
         }
     }
+
 };
 
 #endif // SEARCH_ENGINE_H
